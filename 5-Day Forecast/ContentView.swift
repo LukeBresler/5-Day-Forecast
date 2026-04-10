9c870dc9a160494d8d8a9873a188d3767d796cd1
Luke Bresler <luke@realpixel.co.za>
2026-04-10 08:52:50 +0200

    Initial Commit

---
+++ 5-Day Forecast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  5-Day Forecast
+//
+//  Created by Luke Bresler on 2026/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 5-Day Forecast/__Day_ForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  __Day_ForecastApp.swift
+//  5-Day Forecast
+//
+//  Created by Luke Bresler on 2026/04/10.
+//
+
+import SwiftUI
+
+@main
+struct __Day_ForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
